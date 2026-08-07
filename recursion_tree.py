8a6798f75969ab56f17c18db2f34701284d002dd
Daniel Sanchez <149237002+cheetosanchez@users.noreply.github.com>
2026-08-07 00:11:23 -0700

    don't even care to learn at this point. just cheating my way through

--- recursion_tree.py
+++ recursion_tree.py
@@ -1,4 +1,12 @@
 def list_files(
     parent_directory: dict[str, dict | None], current_filepath: str = ""
 ) -> list[str]:
-    pass
+    filepaths = []
+    for node in parent_directory:
+        next = parent_directory[node]
+        if next is None:
+            filepaths.append(f"{current_filepath}/{node}")
+        else:
+            next_filepaths = list_files(next, f"{current_filepath}/{node}")
+            filepaths.extend(next_filepaths)
+    return filepaths
