d87b1e6c85380cfa9f41cb96d149d61831e4c7c4
Daniel Sanchez <149237002+cheetosanchez@users.noreply.github.com>
2026-08-06 19:42:49 -0700

    copy don't modify

--- pure_functions/ref_v_val.py
+++ pure_functions/ref_v_val.py
@@ -1,8 +1,10 @@
 def add_format(default_formats: dict[str, bool], new_format: str) -> dict[str, bool]:
-    default_formats[new_format] = True
-    return default_formats
+    default = default_formats.copy()
+    default[new_format] = True
+    return default
 
 
 def remove_format(default_formats: dict[str, bool], old_format: str) -> dict[str, bool]:
-    default_formats[old_format] = False
-    return default_formats
+    default = default_formats.copy()
+    default[old_format] = False
+    return default
