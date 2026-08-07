6e0f537c4573bccfa1438367bc1e50510a6bf594
Daniel Sanchez <149237002+cheetosanchez@users.noreply.github.com>
2026-08-07 01:56:41 -0700

    are we enclosing nonlocals over here

--- closures/intro.py
+++ closures/intro.py
@@ -2,4 +2,11 @@ from collections.abc import Callable
 
 
 def word_count_aggregator() -> Callable[[str], int]:
-    pass
+    count: int = 0
+
+    def inner(doc: str) -> int:
+        nonlocal count
+        count += len(doc.split(" "))
+        return count
+
+    return inner
