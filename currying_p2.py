515e3de7e473d29b4078fd711f009b942113ac27
Daniel Sanchez <149237002+cheetosanchez@users.noreply.github.com>
2026-08-07 17:18:27 -0700

    over this shit

--- currying_p2.py
+++ currying_p2.py
@@ -1,32 +1,18 @@
 from collections.abc import Callable
 
-"""
-Complete the create_markdown_image function using currying. It takes 
-a string input, alt_text.
-
-1. Enclose the alt_text in square brackets prefixed with an 
-exclamation point: ![alt_text]
-2. Define an inner function that also takes a string input, url:
-    a. The inner function should first escape any parentheses in the URL by replacing them with encoded sequences.
-        i. Use the .replace() string method to change any opening 
-        parenthesis ( into %28.
-        ii. Do the same to change any closing parenthesis ) into %29.
-    b. Enclose the url with parentheses: (url)
-    c. Add the enclosed url to the end of the enclosed 
-    alt_text: ![alt_text](url)
-    d. Define the innermost function. It should take an optional string input for the title (title=None).
-        i. If a title is passed:
-            - Enclose it in double quotes.
-            - Add the quoted title to the image syntax by first removing 
-            the closing parenthesis ) from the end of the image syntax.
-            - Add a space and the quoted title with a closing parenthesis ) 
-            to the end of the image syntax: ![alt_text](url "title")
-        ii. Return the finished image syntax.
-    e. Return the innermost function.
-3. Return the inner function.
-"""
-
 
 def create_markdown_image(alt_text: str) -> Callable[[str], Callable[..., str]]:
-    # 1. Enclose the alt_text in square brackets prefixed with an exclamation point: ![alt_text]
-    image_syntax = f"![{alt_text}]"
+    enclosed_alt_text = f"![{alt_text}]"
+
+    def add_url(url: str) -> Callable[..., str]:
+        escaped_url = url.replace("(", "%28").replace(")", "%29")
+        image_syntax = enclosed_alt_text + f"({escaped_url})"
+
+        def add_title(title: str | None = None) -> str:
+            if title:
+                return image_syntax[:-1] + f' "{title}")'
+            return image_syntax
+
+        return add_title
+
+    return add_url
